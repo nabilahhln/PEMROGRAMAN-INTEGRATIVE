ca8d61177becd42c69cdacec3accfdf7e8fb86de
nabilahhln <66457622+nabilahhln@users.noreply.github.com>
2021-03-02 10:46:50 +0700

    penjumlahan
    penjumlahan

---
+++ penjumlahan.cpp
@@ -0,0 +1,13 @@
+ #include<iostream>
+using namespace std;
+int main()
+{
+ int a,b,hasil;
+  //nilai
+ a=10;
+ b=10;
+ //proses
+ hasil=a+b;
+  //output
+ cout<<"\nHasil Penjumlahan = "<<hasil<<endl;
+ return 0; }
